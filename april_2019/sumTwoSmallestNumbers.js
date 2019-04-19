99a5644e09985bd589049d5b373fb8d5fe6ccdb6
wpalafox <wpalafox@gmail.com>
2019-04-18 20:09:14 -0500

    sumTwoSmallestNumbers.js complete

---
+++ april_2019/sumTwoSmallestNumbers.js
@@ -0,0 +1,37 @@
+/*Create a function that returns the sum of the two lowest positive numbers given an 
+array of minimum 4 integers. No floats or empty arrays will be passed.
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
+
+
+testArry =[19,5,42,1]
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+     let tempArry = numbers;
+     let addedMinValue = 0;
+//This loops takes the 2 smallest values out of the temporary array and adds tem
+     for(i=0;i<2;i++){ 
+        addedMinValue += parseInt(tempArry.splice(tempArry.indexOf(Math.min(...tempArry)),1))
+      }
+
+   
+
+     return addedMinValue
+
+};
+
+
+
+
+console.log(sumTwoSmallestNumbers(testArry))
+
+
+
+//Best Practices Answer
+
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
